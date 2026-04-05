706a25a267cb02aba3ce2a81e488fad47ef5c39f
Dmytro Svitkin <77505900+Dmytro-Svitkin@users.noreply.github.com>
2026-04-06 01:15:01 +0200

    fn war, fn input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self,Write};
+
+fn war()->String{
+    String::from("~WAR~")
+}
+
+fn input(msg:&str)->String{
+    print!("{}",msg);
+    io::stdout().flush().unwrap();
+    let mut result:String=String::new();
+    io::stdin().read_line(&mut result).unwrap();
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
 }
